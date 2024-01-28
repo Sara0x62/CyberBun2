eb5ddeb041744f77e19f0cf6ff983dbc2e021ff9
Sara0x62 <sara98.sain@gmail.com>
2024-01-28 23:50:28 +0100

    adding some event stuff

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -1,4 +1,6 @@
-use poise::{serenity_prelude::{Context, FullEvent}, FrameworkContext};
+use poise::{serenity_prelude::{ActivityData, Context, FullEvent}, FrameworkContext};
+use std::sync::atomic::Ordering::SeqCst;
+use tracing::info;
 use super::{Data, Error};
 
 
@@ -8,5 +10,58 @@ pub async fn event_handler (
     _framework: FrameworkContext<'_, Data, Error>,
     data: &Data) -> Result<(), Error>
 {
+
+    match event {
+
+        FullEvent::Ready { data_about_bot } => {
+
+            info!("Bot ready! - serving {} server(s)...", data_about_bot.guilds.len());
+            ctx.set_activity( 
+                Some(
+                    ActivityData::watching(format!("{} server(s) - OwO", data_about_bot.guilds.len()))
+                )
+            );
+        },
+
+        FullEvent::GuildCreate { guild: _, is_new } => {
+            match is_new {
+                Some(true) => {
+                    info!("Joined 1 new server!");
+
+                    let new_count = data.server_count.load(SeqCst) + 1;
+                    data.server_count.store(new_count, SeqCst);
+
+                    ctx.set_activity( 
+                        Some(
+                            ActivityData::watching(format!("{} server(s) - OwO", new_count) )
+                        )
+                    );
+                },
+                Some(false) => (),
+                _ => ()
+            }
+        },
+
+        FullEvent::GuildDelete { incomplete, full: _ } => {
+            let new_count = data.server_count.load(SeqCst) - 1;
+            data.server_count.store(new_count, SeqCst);
+
+            match incomplete.unavailable {
+                false => info!("I Got removed from 1 server"),
+                true => info!("A server I was serving became unavailable"),
+            }
+
+            ctx.set_activity( 
+                Some(
+                    ActivityData::watching(format!("{} server(s) - OwO", new_count) )
+                )
+            );
+
+        },
+
+
+        _ => (),
+    }
+
     Ok(())
 }
