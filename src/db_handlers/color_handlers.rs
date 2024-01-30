5a5bfc690a66f54d0a38709fd3c1df4d9137da39
Sara0x62 <sara98.sain@gmail.com>
2024-01-30 10:42:16 +0100

    refactoring some code and getting rid of some duplicate code

--- src/db_handlers/color_handlers.rs
+++ src/db_handlers/color_handlers.rs
@@ -2,19 +2,29 @@ use super::super::Error;
 use sqlx::{pool::PoolConnection, Row, Sqlite};
 use tracing::{info, warn};
 
+#[derive(sqlx::FromRow)]
+pub struct ColorRow {
+    pub role_id: u64,
+    pub uid: u64,
+    pub guid: u64,
+    pub color: u32,
+    pub role_name: String,
+}
 
 pub async fn create_color_role(
-    mut conn: PoolConnection<Sqlite>, 
-    role_id: u64, uid: u64, guid: u64, color: u32, 
-    name: String
+    mut conn: PoolConnection<Sqlite>,
+    role_id: u64,
+    uid: u64,
+    guid: u64,
+    color: u32,
+    name: String,
 ) -> Result<(), Error> {
-
     let result = sqlx::query(
         r#"
         INSERT INTO colors (
             role_id, uid, guid, color, role_name
         ) VALUES (?, ?, ?, ?, ?);
-        "#
+        "#,
     )
     .bind(role_id as i64)
     .bind(uid as i64)
@@ -23,7 +33,7 @@ pub async fn create_color_role(
     .bind(name)
     .execute(&mut *conn)
     .await?;
-    
+
     conn.close().await?;
 
     match result.rows_affected() {
@@ -33,52 +43,61 @@ pub async fn create_color_role(
     Ok(())
 }
 
-pub async fn color_role_exists(
+pub async fn get_color(
     mut conn: PoolConnection<Sqlite>,
-    uid: u64, guid: u64
-) -> Result<(bool, Option<u64>), Error> {
-    
-    let result = sqlx::query(
+    uid: u64,
+    guid: u64,
+) -> Result<Option<ColorRow>, Error> {
+    #[derive(sqlx::FromRow)]
+    struct TempColor {
+        role_id: i64,
+        uid: i64,
+        guid: i64,
+        color: u32,
+        role_name: String,
+    }
+
+    let result = sqlx::query_as::<_, TempColor>(
         r#"
-        SELECT role_id
+        SELECT *
         FROM colors
         WHERE uid = ? AND guid = ?;
-        "#
+        "#,
     )
     .bind(uid as i64)
     .bind(guid as i64)
-    .fetch_one(&mut *conn)
-    .await;
+    .fetch_optional(&mut *conn)
+    .await?;
 
     conn.close().await?;
 
     match result {
-        Ok(row) => {
-            return Ok( (true, Some(row.get::<i64, _>("role_id") as u64)) );
-        },
-        Err(err) => {
-            match err {
-                sqlx::Error::RowNotFound => return Ok ( (false, None) ),
-                _ => warn!("Unexpected error checking if color role exists: {}", err),
-            }
+        Some(result) => {
+            return Ok(Some(ColorRow {
+                role_id: result.role_id as u64,
+                uid: result.uid as u64,
+                guid: result.guid as u64,
+                color: result.color,
+                role_name: result.role_name,
+            }))
         }
-    }
-    
+        None => {}
+    };
 
-    Ok( (false, None) )
+    Ok(None)
 }
 
 pub async fn update_color_role(
     mut conn: PoolConnection<Sqlite>,
-    role_id: u64, color: u32
+    role_id: u64,
+    color: u32,
 ) -> Result<(), Error> {
-
     let result = sqlx::query(
         r#"
         UPDATE colors
         set color = ?
         WHERE role_id = ?;
-        "#
+        "#,
     )
     .bind(color)
     .bind(role_id as i64)
@@ -88,25 +107,4 @@ pub async fn update_color_role(
     conn.close().await?;
 
     Ok(())
-}
-
-pub async fn get_color_code(
-    mut conn: PoolConnection<Sqlite>,
-    uid: u64, guid: u64
-) -> Result<u32, Error> {
-
-    let result = sqlx::query(
-        r#"
-        SELECT color
-        FROM colors
-        WHERE uid = ? AND guid = ?
-        "#)
-        .bind(uid as i64)
-        .bind(guid as i64)
-        .fetch_one(&mut *conn)
-        .await?;
-
-    conn.close().await?;
-
-    return Ok(result.get::<u32, _>("color"));
 }
